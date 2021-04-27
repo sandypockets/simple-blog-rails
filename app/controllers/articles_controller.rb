bc9999df95305085e31db6a5415993e0977afc26
Zac Alexander <zac@znacreative.com>
2021-04-27 16:35:14 +0000

    Set up articles index page to show all articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,14 @@
 class ArticlesController < ApplicationController
-  def new; end
+
+  def index
+    @articles = Article.all
+  end
 
   def show
     @article = Article.find(params[:id])
   end
+
+  def new; end
 
   def create
     @article = Article.new(article_params)
