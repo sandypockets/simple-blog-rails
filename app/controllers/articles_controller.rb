12ed3a07fcd4410db53350ce43320d176383edea
Zac Alexander <zac@znacreative.com>
2021-04-27 16:11:01 +0000

    Add accepted params to the @article creation controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
   def new; end
 
   def create
-    @article = Article.new(params[:article])
+    @article = Article.new(params.require(:article).permit(:title, :text))
 
     @article.save
     redirect_to @article
